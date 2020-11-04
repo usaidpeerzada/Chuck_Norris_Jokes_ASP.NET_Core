adc795d6aaf2aa99133f2f2e70e25753bc533608
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2020-11-04 21:21:57 +0530

    Add project files.

---
+++ BlazorApp1/Models/ChuckNorrisJokesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class ChuckNorrisJokesModel
+    {
+        public object[] Categories { get; set; }
+        public string Created_at { get; set; }
+        public string Icon_url { get; set; }
+        public string Id { get; set; }
+        public string Updated_at { get; set; }
+        public string Url { get; set; }
+        public string Value { get; set; }
+    }
+
+}
